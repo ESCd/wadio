155e7c8b9f82ebe61ea110a2d812d5e6fcc54931
cryptoc1 <steelesam72@gmail.com>
2025-05-30 03:18:18 -0400

    General Update: - [NEW] include url in `ErrorDetails` - [FIX] early cancellation of search type-ahead

--- src/App/UI/Components/ErrorDialog.razor.cs
+++ src/App/UI/Components/ErrorDialog.razor.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
 using Wadio.App.UI.Abstractions;
 
 namespace Wadio.App.UI.Components;
@@ -12,17 +13,21 @@ internal sealed class ErrorDetails
     public string? Source { get; private init; }
     public string? StackTrace { get; private init; }
     public AppVersion Version { get; } = AppVersion.Value;
+    public Uri Url { get; private init; }
 
-    public static ErrorDetails Create( Exception exception )
+    public static ErrorDetails Create( NavigationManager navigation, Exception exception )
     {
+        ArgumentNullException.ThrowIfNull( navigation );
         ArgumentNullException.ThrowIfNull( exception );
+
         return new()
         {
             ApiProblem = (exception as ApiProblemException)?.Problem,
             Message = $"{exception}",
             Source = exception.Source,
             StackTrace = exception.StackTrace,
-            Type = exception.GetType().FullName
+            Type = exception.GetType().FullName,
+            Url = new( navigation.Uri ),
         };
     }
 
