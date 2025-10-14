ccad13d4c50eb3957642b6feafba53cc01b377ed
cryptoc1 <steelesam72@gmail.com>
2025-10-14 06:11:34 -0400

    [NEW] use `superjson` for `LocalStorageInterop`

--- src/App/UI/Interop/LocalStorage.module.ts
+++ src/App/UI/Interop/LocalStorage.module.ts
@@ -1,12 +1,14 @@
+import superjson from 'superjson';
+
 export const get = (key: string) => {
   const value = localStorage.getItem(key);
   if (!value) return null;
 
   try {
-    return JSON.parse(value);
+    return superjson.parse(value);
   } catch {
     return null;
   }
 };
 
-export const set = (key: string, value: any) => localStorage.setItem(key, JSON.stringify(value));
+export const set = (key: string, value: any) => localStorage.setItem(key, superjson.stringify(value));
